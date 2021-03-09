76e18ca2285f2857af1826e6a73804092254bfc3
Ben Steinberg <bsteinberg@law.harvard.edu>
2021-03-09 09:49:20 -0500

    Remove spurious tags from JS

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,4 @@
 <!-- Matomo -->
-<script type="text/javascript">
   var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
@@ -11,5 +10,4 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
-</script>
 <!-- End Matomo Code -->
